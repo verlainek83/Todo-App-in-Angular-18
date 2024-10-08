f1f33eabfd0689ca2e37a222a554b7053aff7dff
Verlaine KOUWE <verlaine_k@outlook.com>
2024-10-08 14:14:04 +0200

    fin de l'application

--- angular_to_do_list/src/app/app.component.ts
+++ angular_to_do_list/src/app/app.component.ts
@@ -1,46 +1,63 @@
-import { NgFor } from '@angular/common';
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgClass, NgFor } from '@angular/common';
 
-export interface TodoItem{
-  id:number;
-  task:string;
-  completed:boolean
+export interface TodoItem {
+  id: number;
+  task: string;
+  completed: boolean;
 }
 
+
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, FormsModule, NgFor],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet,NgFor,NgClass]
 })
 export class AppComponent {
-  todoList: TodoItem [] = [];
-  newTask:string = ''
 
-  addTask():void{
-    if (this.newTask.trim() !== ''){
-      const newTodoItem :TodoItem = {
-        id : Date.now(),
-        task : this.newTask,
-        completed : false
-      }
+  todoList: TodoItem[] = [];
+    newTask: string = '';
+    @ViewChild('todoText') todoInputRef: ElementRef<HTMLInputElement> = null!;
 
-      this.todoList.push(newTodoItem)
-      this.newTask = ''
-      console.log(this.todoList);
+    ngOnInit(): void {
+      const storedTodoList = this.todoList;
+    //   if (storedTodoList) {
+    //       this.todoList = JSON.parse(storedTodoList);
+    //   }
+  }
+  addTask(text: string): void {
+    if (text.trim() !== '') {
+        const newTodoItem: TodoItem = {
+            id: Date.now(),
+            task: text.trim(),
+            completed: false
+        };
+        this.todoList.push(newTodoItem);
+        this.todoInputRef.nativeElement.value = '';
+        this.saveTodoList();
     }
-  }
-  toggleCompleted(index: number):void{
-    console.log(index);
-    this.todoList[index].completed = !this.todoList[index].completed
-    console.log(this.todoList);    
-  }
-  deleteTask(id:number):void{
-    this.todoList = this.todoList.filter(item => item.id !== id)
-    console.log(this.todoList);
-    
-  }
+}
+
+deleteTask(id: number): void {
+    this.todoList = this.todoList.filter(item => item.id !== id);
+    this.saveTodoList();
+}
+
+toggleCompleted(id: number): void {
+    const todoItem = this.todoList.find(item => item.id === id);
+    if (todoItem) {
+        todoItem.completed = !todoItem.completed;
+        this.saveTodoList();
+    }
+}
+
+saveTodoList(): void {
+    // localStorage.setItem('todoList', JSON.stringify(this.todoList));
+
+}
+
+
 }
