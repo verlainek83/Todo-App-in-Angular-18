a2988cf9351a0ba668332df9aad5acabe3eae9d7
Verlaine KOUWE <verlaine_k@outlook.com>
2024-10-08 13:01:58 +0200

    création de la fonction add et delete

--- angular_to_do_list/src/app/app.component.ts
+++ angular_to_do_list/src/app/app.component.ts
@@ -1,21 +1,46 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 export interface TodoItem{
   id:number;
-  task:ScrollSetting;
+  task:string;
   completed:boolean
 }
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule],
+  imports: [RouterOutlet, FormsModule, NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   todoList: TodoItem [] = [];
   newTask:string = ''
+
+  addTask():void{
+    if (this.newTask.trim() !== ''){
+      const newTodoItem :TodoItem = {
+        id : Date.now(),
+        task : this.newTask,
+        completed : false
+      }
+
+      this.todoList.push(newTodoItem)
+      this.newTask = ''
+      console.log(this.todoList);
+    }
+  }
+  toggleCompleted(index: number):void{
+    console.log(index);
+    this.todoList[index].completed = !this.todoList[index].completed
+    console.log(this.todoList);    
+  }
+  deleteTask(id:number):void{
+    this.todoList = this.todoList.filter(item => item.id !== id)
+    console.log(this.todoList);
+    
+  }
 }
